7ec23b515335ecc50e50bf0f577fdf65d99299f9
sngom1995 <ngomsamba75@gmail.com>
2023-04-19 13:31:47 +0000

    add author and book repositories

---
+++ src/main/java/sam/guru/spring6webapp/reposirories/AuthorRepository.java
@@ -0,0 +1,6 @@
+package sam.guru.spring6webapp.reposirories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<AuthorRepository, Long> {
+}
